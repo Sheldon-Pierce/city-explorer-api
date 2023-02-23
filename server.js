2e30ad34f2fe0fa0ce6a5dc0bbf755aa6cbb1e7f
Sheldon Pierce <Pierce55@icloud.com>
2023-02-22 23:00:43 -0800

    Fixed minor things

--- server.js
+++ server.js
@@ -17,24 +17,23 @@ app.use(cors());
 class Forecast {
     constructor(description, date) {
         this.description = description,
-        this.date = date
+            this.date = date
     }
 }
 
 
 app.get('/weather', async (request, response) => {
 
-        if(request.query.lat) {
-            let proxy = {
-                url: `https://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.lat}&lon=${request.query.lon}&key=${TOKEN}`,
-                method: 'GET',
-            };
-            let proxyRes =  await axios(proxy)
-            response.status(200).send(proxyRes.data);
-        }
-        });
+    let proxy = {
+        url: `https://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.lat}&lon=${request.query.lon}&key=${TOKEN}`,
+        method: 'GET',
+    };
+    let proxyRes = await axios(proxy)
+    response.status(200).send(proxyRes.data);
 
-app.get('/movies', async(request, response) => {
+});
+
+app.get('/movies', async (request, response) => {
     let movies = {
         url: `https://api.themoviedb.org/3/search/movie?api_key=${MTOKEN}&language=en-US&query=${request.query.query}&page=1&include_adult=false`,
         method: `GET`,
