90974da26221a3245e26004c7c8fd74b26f1b1ef
Sheldon Pierce <Pierce55@icloud.com>
2023-02-23 16:59:01 -0800

    Completed modulation of the backend

--- server.js
+++ server.js
@@ -4,43 +4,18 @@ const axios = require('axios');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const weather = require('./data/weather.json');
+const getWeather = require('./lib/weather');
+const getMovies = require('./lib/movies');
 const app = express();
 
 
 const PORT = process.env.PORT;
-const TOKEN = process.env.WEATHER_API_KEY
-const MTOKEN = process.env.MOVIE_API_KEY
 
 app.use(cors());
 
-class Forecast {
-    constructor(description, date) {
-        this.description = description,
-            this.date = date
-    }
-}
+app.get('/weather', getWeather);
 
-
-app.get('/weather', async (request, response) => {
-
-    let proxy = {
-        url: `https://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.lat}&lon=${request.query.lon}&key=${TOKEN}`,
-        method: 'GET',
-    };
-    let proxyRes = await axios(proxy)
-    response.status(200).send(proxyRes.data);
-
-});
-
-app.get('/movies', async (request, response) => {
-    let movies = {
-        url: `https://api.themoviedb.org/3/search/movie?api_key=${MTOKEN}&language=en-US&query=${request.query.query}&page=1&include_adult=false`,
-        method: `GET`,
-    }
-    let moviesRes = await axios(movies)
-    response.status(200).send(moviesRes.data)
-});
+app.get('/movies', getMovies);
 
 
 app.listen(PORT, () => {
